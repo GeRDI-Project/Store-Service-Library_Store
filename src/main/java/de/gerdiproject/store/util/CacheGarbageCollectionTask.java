71126d7c2ec645b684016263d24ea21e42cdf75b
Nelson Tavares de Sousa <ntd@informatik.uni-kiel.de>
2018-09-21 14:08:07 +0200

    SAI-1148 added a logger to the collector

--- src/main/java/de/gerdiproject/store/util/CacheGarbageCollectionTask.java
+++ src/main/java/de/gerdiproject/store/util/CacheGarbageCollectionTask.java
@@ -2,14 +2,17 @@ package de.gerdiproject.store.util;
 
 import de.gerdiproject.store.datamodel.CacheElement;
 import de.gerdiproject.store.datamodel.ICredentials;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.Instant;
-import java.time.temporal.TemporalUnit;
 import java.util.Map;
 import java.util.TimerTask;
 
 public class CacheGarbageCollectionTask<E extends ICredentials> extends TimerTask {
 
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(CacheGarbageCollectionTask.class);
     private final Map<String, CacheElement<E>> map;
 
     public CacheGarbageCollectionTask(Map<String, CacheElement<E>> map) {
@@ -18,12 +21,16 @@ public class CacheGarbageCollectionTask<E extends ICredentials> extends TimerTas
 
     @Override
     public void run() {
+        LOGGER.debug("CacheGarbageCollector is executed");
+        int count = 0;
         final Instant halfAnHourAgo = Instant.now().minusSeconds(1800);
         for (Map.Entry<String, CacheElement<E>> entry : map.entrySet()) {
             if (entry.getValue().getTimespamp().isBefore(halfAnHourAgo)) {
                 this.map.remove(entry.getKey());
+                count++;
             }
         }
+        LOGGER.info("Removed " + count + " old session from the cache.");
     }
 
 }
