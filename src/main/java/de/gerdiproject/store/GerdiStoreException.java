e739282b7cd2487e01a3e1ed18f8750a3743ad90
Nelson Tavares de Sousa <ntd@informatik.uni-kiel.de>
2019-05-07 14:24:02 +0200

    SAI-1601 added javadoc and license header

--- src/main/java/de/gerdiproject/store/GerdiStoreException.java
+++ src/main/java/de/gerdiproject/store/GerdiStoreException.java
@@ -1,5 +1,25 @@
+/**
+ * Copyright © 2019 Nelson Tavares de Sousa (tavaresdesousa@email.uni-kiel.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.gerdiproject.store;
 
+/**
+ * This class represents an Exception which may be thrown by the store service library, if an unexpected error occurs during the initialization.
+ *
+ * @author Nelson Tavares de Sousa
+ */
 public class GerdiStoreException extends RuntimeException {
 
     public GerdiStoreException(String msg, Throwable e) {
